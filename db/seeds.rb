24e66ad29bfb49b3baedc4d0b324fb4eb7fdfb62
Abe G <abenbg12@gmail.com>
2025-02-19 16:50:58 -0500

    Completed task that geerates list of flats, with user being able to add or remove flats

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+4.times do
+  Flat.create!(
+    name: Faker::Book.title,
+    address: Faker::Address.street_address,
+    description: Faker::Movies::Hobbit.quote,
+    price_per_night: Faker::Number.decimal_part(digits: 2),
+    number_of_guests: Faker::Number.decimal_part(digits: 1)
+  )
+  p "saved!"
+end
